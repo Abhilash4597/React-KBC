44c09312985eca436dfcb0478619a22c635a17c8
Abhilash Singh <abhilash4597@gmail.com>
2024-02-08 19:04:05 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,74 @@ function App() {
   return (
     <div className='app'>
       <div className='main'>Main</div>
-      <div className='money'>Money</div>
+      <div className='money'>
+        <ul className='money_list'>
+          <li className='money_list_item'>
+            <span className='money_list_item_number'>16</span>
+            <span className='money_list_item_amount'>70000000</span>
+          </li>
+          <li className='money_list_item active'>
+            <span className='money_list_item_number'>15</span>
+            <span className='money_list_item_amount'>10000000</span>
+          </li>
+          <li className='money_list_item'>
+            <span className='money_list_item_number'>14</span>
+            <span className='money_list_item_amount'>5000000</span>
+          </li>
+          <li className='money_list_item'>
+            <span className='money_list_item_number'>13</span>
+            <span className='money_list_item_amount'>2500000</span>
+          </li>
+          <li className='money_list_item'>
+            <span className='money_list_item_number'>12</span>
+            <span className='money_list_item_amount'>1250000</span>
+          </li>
+          <li className='money_list_item'>
+            <span className='money_list_item_number'>11</span>
+            <span className='money_list_item_amount'>640000</span>
+          </li>
+          <li className='money_list_item'>
+            <span className='money_list_item_number'>10</span>
+            <span className='money_list_item_amount'>320000</span>
+          </li>
+          <li className='money_list_item'>
+            <span className='money_list_item_number'>9</span>
+            <span className='money_list_item_amount'>160000</span>
+          </li>
+          <li className='money_list_item'>
+            <span className='money_list_item_number'>8</span>
+            <span className='money_list_item_amount'>80000</span>
+          </li>
+          <li className='money_list_item'>
+            <span className='money_list_item_number'>7</span>
+            <span className='money_list_item_amount'>40000</span>
+          </li>
+          <li className='money_list_item'>
+            <span className='money_list_item_number'>6</span>
+            <span className='money_list_item_amount'>20000</span>
+          </li>
+          <li className='money_list_item'>
+            <span className='money_list_item_number'>5</span>
+            <span className='money_list_item_amount'>10000</span>
+          </li>
+          <li className='money_list_item'>
+            <span className='money_list_item_number'>4</span>
+            <span className='money_list_item_amount'>5000</span>
+          </li>
+          <li className='money_list_item'>
+            <span className='money_list_item_number'>3</span>
+            <span className='money_list_item_amount'>3000</span>
+          </li>
+          <li className='money_list_item'>
+            <span className='money_list_item_number'>2</span>
+            <span className='money_list_item_amount'>2000</span>
+          </li>
+          <li className='money_list_item'>
+            <span className='money_list_item_number'>1</span>
+            <span className='money_list_item_amount'>1000</span>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
