e491424619d1b5c0af5b8cd93abef0a134d16569
Abhilash Singh <abhilash4597@gmail.com>
2024-02-08 14:51:58 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className='app'>
+      <div className='main'>Main</div>
+      <div className='money'>Money</div>
+    </div>
   )
 }
 
