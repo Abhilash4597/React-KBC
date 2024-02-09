7426c63d453a6b11e74e24f23603a49ec18cfd96
Abhilash Singh <abhilash4597@gmail.com>
2024-02-10 00:21:31 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import "./App.css";
 import Quiz from "./Components/Quiz";
 import data from "./Data/data.json";
+import Timer from "./Components/Timer";
 
 function App() {
   const [questionNum, setQuestionNum] = useState(1);
@@ -10,24 +11,28 @@ function App() {
 
   // console.log(data[0].question)
 
-  const moneyPyramid = [
-    { id: 1, amount: "1000" },
-    { id: 2, amount: "2000" },
-    { id: 3, amount: "3000" },
-    { id: 4, amount: "5000" },
-    { id: 5, amount: "10000" },
-    { id: 6, amount: "20000" },
-    { id: 7, amount: "40000" },
-    { id: 8, amount: "80000" },
-    { id: 9, amount: "160000" },
-    { id: 10, amount: "320000" },
-    { id: 11, amount: "640000" },
-    { id: 12, amount: "1250000" },
-    { id: 13, amount: "2500000" },
-    { id: 14, amount: "5000000" },
-    { id: 15, amount: "10000000" },
-    { id: 16, amount: "70000000" },
-  ].reverse();
+  const moneyPyramid = useMemo(
+    () =>
+      [
+        { id: 1, amount: "1000" },
+        { id: 2, amount: "2000" },
+        { id: 3, amount: "3000" },
+        { id: 4, amount: "5000" },
+        { id: 5, amount: "10000" },
+        { id: 6, amount: "20000" },
+        { id: 7, amount: "40000" },
+        { id: 8, amount: "80000" },
+        { id: 9, amount: "160000" },
+        { id: 10, amount: "320000" },
+        { id: 11, amount: "640000" },
+        { id: 12, amount: "1250000" },
+        { id: 13, amount: "2500000" },
+        { id: 14, amount: "5000000" },
+        { id: 15, amount: "10000000" },
+        { id: 16, amount: "70000000" },
+      ].reverse(),
+    []
+  );
 
   useEffect(() => {
     questionNum > 1 &&
@@ -44,7 +49,9 @@ function App() {
         ) : (
           <>
             <div className="top">
-              <div className="timer">30</div>
+              <div className="timer">
+                <Timer setStop={setStop} questionNum={questionNum} />
+              </div>
             </div>
             <div className="bottom">
               <Quiz
