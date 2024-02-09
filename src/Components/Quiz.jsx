8a87a65a54c4bf6ebb5541d87876252ab6302c5c
Abhilash Singh <abhilash4597@gmail.com>
2024-02-09 12:32:17 +0530

    update

--- src/Components/Quiz.jsx
+++ src/Components/Quiz.jsx
@@ -1,28 +1,41 @@
-import { useEffect, useState } from "react"
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
 
-export default function Quiz({data , setimeOut , setQuestionNum , questionNum}) {
+export default function Quiz({ data, setimeOut, setQuestionNum, questionNum }) {
+  const [question, setQuestion] = useState(null);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [className, setClassName] = useState(null);
 
-    const [questions , setQuestions] = useState(null);
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * data.length);
+    const randomQuestion = data[randomIndex];
+    setQuestion(randomQuestion);
+  }, [data, questionNum]);
 
-    useEffect(()=>{
-        setQuestions(data[Math.floor(Math.random() *40)]);
-    },[data, questionNum])
-
+  const handleClick = (option) => {
+    setSelectedAnswer(option.option);
+    setClassName("answer active");
+    console.log(option.option);
+  };
 
   return (
-    <div className="quiz">
-        {questions ?questions.map((question,id)=>{
-            return <div key={id}>
-            <div className="questions">{question.question}</div>
-            <div className="answers">
-                <div className="answer">Abhilash Singh</div>
-                <div className="answer">Abhilash Singh</div>
-                <div className="answer">Abhilash Singh</div>
-                <div className="answer">Abhilash Singh</div>
-            </div>
-            </div>
-        })}
-        
+    <div>
+      {question && (
+        <div className="quiz">
+          <div className="questions">{question.question}</div>
+          <div className="answers">
+            {question.options.map((option, id) => (
+              <div
+                key={id}
+                className={selectedAnswer===option.option ? className:'answer'}
+                onClick={() => handleClick(option)}>
+                {option.option}
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
